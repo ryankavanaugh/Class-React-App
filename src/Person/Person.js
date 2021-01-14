abb462be0270b51693cad7e5a087505f195dc91f
Ryan Kavanaugh <ryankavanaugh@Ryans-MBP.lan>
2021-01-13 19:23:00 -0600

    update

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -7,11 +7,10 @@ import React from 'react'
 const Person = (props) => {
     return (
         <div>
-            <p>I'm {props.name} and I am {props.age} years old!</p>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
         </div>
     )
-    
 };
 
 export default Person;
